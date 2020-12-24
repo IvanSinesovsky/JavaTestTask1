d12437d5c1da05ac067c799fafc51043f2f52cca
IvanSinesovsky <isinegovsky@gmail.com>
2020-12-25 01:09:16 +0300

    added UID

--- src/User.java
+++ src/User.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
  * Class for User description
  */
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
     private static int id = 100_000;
     private int        userId;
     private String     name;
